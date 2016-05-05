a369f4e413ce0691e161f235814177c3e2e8c539
Barbaros Yıldırım <barbaros.yildirim@markafoni.com>
2016-05-05 14:53:49 +0300

    dungeon with game engine

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import kivent_core
+import kivent_cymunk
 from kivy.utils import get_color_from_hex
 from kivy.lang import Builder
 from kivy.config import Config
@@ -7,58 +9,164 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.label import Label
 from kivy.uix.scatterlayout import ScatterLayout
+from kivy.uix.widget import Widget
+from kivy.clock import Clock
+from kivy.core.window import Window
+from kivent_core.gameworld import GameWorld
+from kivent_core.managers.resource_managers import texture_manager
+from kivent_core.systems.renderers import RotateRenderer
+from kivent_core.systems.position_systems import PositionSystem2D
+from kivent_core.systems.rotate_systems import RotateSystem2D
+from kivent_cymunk.interaction import CymunkTouchSystem
+from kivy.properties import StringProperty, NumericProperty
+from math import radians, pi, sin, cos
+
+from random import randint, choice
+from functools import partial
+from os.path import dirname, join, abspath
 
 from maze import Maze
 
 __version__ = '0.0.1'
 
-class Hero():
-    pass
 
-class Dungeon(GridLayout):
+texture_manager.load_atlas(join(dirname(abspath(__file__)),
+                                'assets', 'dungeon.atlas'))
+
+
+class Dungeon(Widget):
+    def __init__(self, **kwargs):
+        super(Dungeon, self).__init__(**kwargs)
+        self.gameworld.init_gameworld(
+            ['cymunk_physics', 'rotate_renderer', 'rotate', 'position',
+             # 'cymunk_touch',
+             'camera1'],
+            callback=self.init_game)
+        self.mazeobj = Maze()
+        self.mazeobj.makeMap(50, 50, 100, 10, 30)
+        self.maze = self.mazeobj.mapArr
+
+    def init_game(self):
+        self.setup_states()
+        self.set_state()
+        self.draw_some_stuff()
+
+    def draw_some_stuff(self):
+        gameview = self.gameworld.system_manager['camera1']
+        # x, y = int(-gameview.camera_pos[0]), int(-gameview.camera_pos[1])
+        # w, h = int(gameview.size[0] + x), int(gameview.size[1] + y)
+        create_wall = self.create_wall
+        for y in range(50):
+            for x in range(50):
+                if self.maze[y][x] in [1, 2]:
+                    create_wall((50*x, 50*y))
+        # gameview.focus_entity = True
+        # gameview.entity_to_focus = None
+        # self.mazeobj.printt()
+        
+    def create_wall(self, pos):
+        x_vel = y_vel = 0
+        angle = radians(0)
+        angular_velocity = radians(0)
+        shape_dict = {'width': 50, 'height': 50, 'mass': 1}
+        col_shape = {'shape_type': 'box', 'elasticity': .5,
+                     'collision_type': 1, 'shape_info': shape_dict,
+                     'friction': 1.0}
+        col_shapes = [col_shape]
+        physics_component = {
+            'main_shape': 'circle',
+            'velocity': (x_vel, y_vel),
+            'position': pos, 'angle': angle,
+            'angular_velocity': angular_velocity,
+            'vel_limit': 0,
+            'ang_vel_limit': radians(0),
+            'mass': 10, 'col_shapes': col_shapes}
+        create_component_dict = {
+            'cymunk_physics': physics_component,
+            'rotate_renderer': {
+                'texture': 'wall',
+                'size': (50, 50),
+                'render': True},
+            'position': pos, 'rotate': 0, }
+        component_order = [
+            'position', 'rotate', 'rotate_renderer', 'cymunk_physics']
+        return self.gameworld.init_entity(
+            create_component_dict, component_order)
+
+    def update(self, dt):
+        self.gameworld.update(dt)
+
+    def setup_states(self):
+        self.gameworld.add_state(
+            state_name='main',
+            systems_added=['rotate_renderer'],
+            systems_removed=[], systems_paused=[],
+            systems_unpaused=['rotate_renderer'],
+            screenmanager_screen='main')
+
+    def set_state(self):
+        self.gameworld.state = 'main'
+
+
+class DebugPanel(Widget):
+    fps = StringProperty(None)
 
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.maze = Maze()
-        self.maze.makeMap(100, 100, 100, 50, 300)
-        self.maze = self.maze.mapArr
-        # for y in range(100):
-        #     line = ""
-        #     for x in range(100):
-        #         if self.maze[y][x] == 0:
-        #             line += " "
-        #         if self.maze[y][x] == 1:
-        #             line += "#"
-        #         if self.maze[y][x] == 2:
-        #             line += "#"
-        #         if self.maze[y][x] == 3 or self.maze[y][x] == 4 or self.maze[y][x] == 5:
-        #             line += " "
-        #     print(line)
-        self.position_maze()
+        super(DebugPanel, self).__init__(**kwargs)
+        Clock.schedule_once(self.update_fps)
 
-    def position_maze(self, start_y=0, start_x=0):
-        for line in self.maze[start_y:start_y + 20]:
-            for j in line[start_x:start_x + 20]:
-                lbl = Label(text="")
-                self.add_widget(lbl)
-                if j in [1, 2]:
-                    lbl.image.source = "assets/wall.jpg"
-                
+    def update_fps(self, dt):
+        self.fps = str(int(Clock.get_fps()))
+        Clock.schedule_once(self.update_fps, .05)
 
-class DungeonGame(GridLayout):
-    pass
 
 class DungeonApp(App):
+    count = NumericProperty(0)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        Builder.load_file('assets/dungeon.kv')
         self.title = "Dungeon Explorer"
 
-    def build(self):
-        game = DungeonGame()
-        return game
 
 if __name__ == '__main__':
-    Config.set('kivy', 'desktop', 1)
     DungeonApp().run()
+
+# class Hero():
+#     pass
+
+# class Dungeon(GridLayout):
+
+#     def __init__(self, **kwargs):
+#         super().__init__(**kwargs)
+#         self.cols = self.rows = 50
+#         self.maze = Maze()
+#         self.maze.makeMap(self.rows, self.cols, 100, 10, 30)
+#         self.maze = self.maze.mapArr
+#         self.position_maze()
+
+#     def position_maze(self, start_y=0, start_x=0):
+#         for line in self.maze:
+#             for j in line:
+#                 lbl = Label(text="")
+#                 self.add_widget(lbl)
+#                 if j in [1, 2]:
+#                     lbl.image.source = "assets/wall.jpg"
+                
+
+# class DungeonGame(GridLayout):
+#     pass
+
+# class DungeonApp(App):
+
+#     def __init__(self, **kwargs):
+#         super().__init__(**kwargs)
+#         Builder.load_file('assets/dungeon.kv')
+#         self.title = "Dungeon Explorer"
+
+#     def build(self):
+#         game = DungeonGame()
+#         return game
+
+# if __name__ == '__main__':
+#     Config.set('kivy', 'desktop', 1)
+#     DungeonApp().run()
--- maze.py
+++ maze.py
@@ -10,6 +10,7 @@ class Maze:
     def __init__(self):
         self.roomList = []
         self.cList = []
+        self.corridorList = []
 
     def makeMap(self, xsize, ysize, fail, b1, mrooms):
         """Generate random layout of rooms, corridors and other features"""
@@ -25,49 +26,133 @@ class Maze:
                 tmp.append(1)
             self.mapArr.append(tmp)
 
-        w, l, t = self.makeRoom()
-        while len(self.roomList) == 0:
-            y = randrange(ysize - 1 - l) + 1
-            x = randrange(xsize - 1 - w) + 1
-            p = self.placeRoom(l, w, x, y, xsize, ysize, 6, 0)
         failed = 0
-        while failed < fail:  # The lower the value that failed< , the smaller the dungeon
-            chooseRoom = randrange(len(self.roomList))
-            ex, ey, ex2, ey2, et = self.makeExit(chooseRoom)
-            feature = randrange(100)
-            if feature < b1:  # Begin feature choosing (more features to be added here)
-                w, l, t = self.makeCorridor()
-            else:
-                w, l, t = self.makeRoom()
-            roomDone = self.placeRoom(l, w, ex2, ey2, xsize, ysize, t, et)
-            if roomDone == 0:  # If placement failed increase possibility map is full
+
+        while failed < fail:
+            w, l, t = self.makeRoom()
+            ey2 = randrange(ysize - 1 - l) + 1
+            ex2 = randrange(xsize - 1 - w) + 1
+            roomDone = self.placeRoom(l, w, ex2, ey2, xsize, ysize, t, 0)
+            if roomDone == 0:
                 failed += 1
-            elif roomDone == 2:  # Possiblilty of linking rooms
-                if self.mapArr[ey2][ex2] == 0:
-                    if randrange(100) < 7:
-                        self.makePortal(ex, ey)
-                    failed += 1
-            else:  # Otherwise, link up the 2 rooms
+            elif roomDone == 1:
+                ex, ey, ex2, ey2, et = self.makeExit(len(self.roomList)-1)
                 self.makePortal(ex, ey)
-                failed = 0
-                if t < 5:
-                    tc = [len(self.roomList) - 1, ex2, ey2, t]
-                    self.cList.append(tc)
-                    self.joinCorridor(len(self.roomList) - 1, ex2, ey2, t, 50)
-            if len(self.roomList) == mrooms:
-                failed = fail
+
+        while failed < 2500:
+            w, l, t = self.makeCorridor()
+            ey2 = randrange(ysize - 1 - l) + 1
+            ex2 = randrange(xsize - 1 - w) + 1
+            roomDone = self.placeRoom(l, w, ex2, ey2, xsize, ysize, t, 0)
+            if roomDone == 0:
+                failed += 1
+
         self.finalJoins()
+        self.joinAll()
+        self.deleteDeadEnd()
+        self.check_reacability()
+        checked = True
+        for y in range(self.size_y):
+            for x in range(self.size_x):
+                if self.mapArr[y][x] == 0:
+                    checked = False
+                    break
+            if not checked:
+                break
+        if not checked:
+            self.makeMap(xsize, ysize, fail, b1, mrooms)
+            
+    def deleteDeadEnd(self):
+        ranges = [[range(self.size_y), range(self.size_x)],
+                  [range(self.size_y - 1, -1, -1), range(self.size_x - 1, -1, -1)],
+                  [range(self.size_y - 1, -1, -1), range(self.size_x)],
+                  [range(self.size_y), range(self.size_x - 1, -1, -1)]
+        ]
+        for yrng, xrng in ranges:
+            for y in yrng:
+                for x in xrng:
+                    movement_area = []
+                    if self.mapArr[y][x] == 0:
+                        neighbours = [[y - 1, x], [y + 1, x], [y, x - 1], [y, x + 1]]
+                        try:
+                            movement_area = list(filter(lambda x: self.mapArr[x[0]][x[1]] == 0, neighbours))
+                        except:
+                            pass
+                        finally:
+                            if len(movement_area) < 2:
+                                self.mapArr[y][x] = 1
+
+    def check_reacability(self, point=None):
+        if not point:
+            found = False
+            for y in range(self.size_y):
+                for x in range(self.size_x):
+                    if self.mapArr[y][x] == 0:
+                        self.check_reacability(point=(y, x))
+                        found = True
+                        break
+                if found:
+                    break
+        else:
+            y, x = point
+            if self.mapArr[y][x] == 0:
+                self.mapArr[y][x] = 8
+                neighbours = [[y - 1, x], [y + 1, x], [y, x - 1], [y, x + 1]]
+                
+                for pnt in neighbours:
+                    self.check_reacability(point=pnt)
+        
+    def joinAll(self):
+        # room -> (h, w, x ,y)
+        united = self.roomList
+        for corridor in self.corridorList:
+            try:
+                united.index(corridor)
+            except ValueError:
+                united.append(corridor)
+        for room in united:
+            headings = [0, 1, 2, 3]
+            trial = 100
+            while headings and trial:
+                heading = choice(headings)
+                if heading == 0:  # North wall
+                    rx = randrange(room[1]) + room[2]
+                    ry = room[3] - 1
+                    rx2 = rx
+                    ry2 = ry - 1
+                elif heading == 1:  # East wall
+                    ry = randrange(room[0]) + room[3]
+                    rx = room[2] + room[1]
+                    rx2 = rx + 1
+                    ry2 = ry
+                elif heading == 2:  # South wall
+                    rx = randrange(room[1]) + room[2]
+                    ry = room[3] + room[0]
+                    rx2 = rx
+                    ry2 = ry + 1
+                elif heading == 3:  # West wall
+                    ry = randrange(room[0]) + room[3]
+                    rx = room[2] - 1
+                    rx2 = rx - 1
+                    ry2 = ry
+                if ry2 < self.size_y and rx2 < self.size_x and self.mapArr[ry2][rx2] == 0:
+                    self.mapArr[ry][rx] = 0
+                    headings.pop(headings.index(heading))
+                else:
+                    trial -= 1
+                # if self.mapArr[ry][rx] == 2:  # If space is a wall, exit
+                #    self.mapArr[ry][rx]
 
     def makeRoom(self):
         """Randomly produce room size"""
         rtype = 5
-        rwide = randrange(5, 15)
-        rlong = randrange(5, 15)
+        rwide = randrange(5, 10)
+        rlong = randrange(5, 10)
         return rwide, rlong, rtype
 
     def makeCorridor(self):
         """Randomly produce corridor length and heading"""
-        clength = randrange(18) + 3
+        clength = randrange(3, 21)
         heading = randrange(4)
         if heading == 0:  # North
             wd = 1
@@ -104,6 +189,17 @@ class Maze:
                 ypos -= offset
         # Then check if there is space
         canPlace = 1
+        if rty != 5:
+
+            ww = ((ww + xpos + 1) - (xsize - 1)) > 0 and ww - ((ww + xpos + 1) - (xsize - 1)) or ww
+            ll = ((ll + ypos + 1) - ysize) > 0 and ll - ((ll + ypos + 1) - ysize) or ll
+            if ww <= 0:
+                canPlace = 0
+                return canPlace
+            if ll <= 0:
+                canPlace = 0
+                return canPlace
+
         if ww + xpos + 1 > xsize - 1 or ll + ypos + 1 > ysize:
             canPlace = 0
             return canPlace
@@ -119,6 +215,7 @@ class Maze:
         if canPlace == 1:
             temp = [ll, ww, xpos, ypos]
             self.roomList.append(temp)
+            self.corridorList.extend(rty == 5 and [] or [temp])
             for j in range(ll + 2):  # Then build walls
                 for k in range(ww + 2):
                     self.mapArr[(ypos - 1) + j][(xpos - 1) + k] = 2
@@ -130,35 +227,65 @@ class Maze:
     def makeExit(self, rn):
         """Pick random wall and random point along that wall"""
         room = self.roomList[rn]
-        while True:
-            rw = randrange(4)
+        x_range = list(range(room[1]))
+        y_range = list(range(room[0]))
+        rw_range = list(range(4))
+        found = False
+        while rw_range:
+            rw = choice(rw_range)
             if rw == 0:  # North wall
-                rx = randrange(room[1]) + room[2]
+                rx = choice(x_range) + room[2]
                 ry = room[3] - 1
-                rx2 = rx
+                rx2 = rx3 = rx
                 ry2 = ry - 1
+                ry3 = ry - 2
             elif rw == 1:  # East wall
-                ry = randrange(room[0]) + room[3]
+                ry = choice(y_range) + room[3]
                 rx = room[2] + room[1]
                 rx2 = rx + 1
-                ry2 = ry
+                rx3 = rx + 2
+                ry2 = ry3 = ry
             elif rw == 2:  # South wall
-                rx = randrange(room[1]) + room[2]
+                rx = choice(x_range) + room[2]
                 ry = room[3] + room[0]
-                rx2 = rx
+                rx2 = rx3 = rx
                 ry2 = ry + 1
+                ry3 = ry + 2
             elif rw == 3:  # West wall
-                ry = randrange(room[0]) + room[3]
+                ry = choice(y_range) + room[3]
                 rx = room[2] - 1
                 rx2 = rx - 1
-                ry2 = ry
-            if self.mapArr[ry][rx] == 2:  # If space is a wall, exit
+                rx3 = rx + 2
+                ry2 = ry3 = ry
+            if (rx2 < self.size_x and
+                ry2 < self.size_y and
+                rx3 < self.size_x and
+                ry3 < self.size_y and
+                1 < ry < self.size_y - 3 and
+                1 < rx < self.size_x - 3 and
+                self.mapArr[ry][rx] == 2):  # If space is a wall, exit)
+                found = True
                 break
+            else:
+                if rw in [0, 2]:
+                    x_range.pop(x_range.index((rx - room[2])))
+                    if not x_range:
+                        rw_range.pop(rw_range.index(0))
+                        rw_range.pop(rw_range.index(2))
+                if rw in [1, 3]:
+                    y_range.pop(y_range.index((ry - room[3])))
+                    if not y_range:
+                        rw_range.pop(rw_range.index(1))
+                        rw_range.pop(rw_range.index(3))
+        if not found:
+            print(room)
+            print("delete room index:{}".format(rn))
+                
         return rx, ry, rx2, ry2, rw
 
     def makePortal(self, px, py):
         """Create doors in walls"""
-        ptype = randrange(100)
+        ptype = randrange(30)
         if ptype > 90:  # Secret door
             self.mapArr[py][px] = 5
             return
@@ -229,25 +356,32 @@ class Maze:
     def finalJoins(self):
         """Final stage, loops through all the corridors to see if any can be joined to other rooms"""
         for x in self.cList:
-            self.joinCorridor(x[0], x[1], x[2], x[3], 10)
+            self.joinCorridor(x[0], x[1], x[2], x[3], 100)
 
+    def printt(self):
+        for y in range(self.size_y):
+            line = ""
+            line2 = ""
+            for x in range(self.size_x):
+                if self.mapArr[y][x] == 0:
+                    line += " "
+                    line2 += "0"
+                if self.mapArr[y][x] == 1:
+                    line += "#"
+                    line2 += "1"
+                if self.mapArr[y][x] == 2:
+                    line += "#"
+                    line2 += "2"
+                if self.mapArr[y][x] in [3,4,5,8]:
+                    line += " "
+                    line2 += str(self.mapArr[y][x])
+            print(line, line2)
 # ----------------------------------------------------------------------------
 # Main
 # ----------------------------------------------------------------------------
 if __name__ == "__main__":
-    startx = 100   # map width
-    starty = 100   # map height
+    startx = 50   # map width
+    starty = 50   # map height
     themap = Maze()
-    themap.makeMap(startx, starty, 100, 50, 300)
-    for y in range(starty):
-        line = ""
-        for x in range(startx):
-            if themap.mapArr[y][x] == 0:
-                line += " "
-            if themap.mapArr[y][x] == 1:
-                line += "#"
-            if themap.mapArr[y][x] == 2:
-                line += "#"
-            if themap.mapArr[y][x] == 3 or themap.mapArr[y][x] == 4 or themap.mapArr[y][x] == 5:
-                line += " "
-        print(line)
+    themap.makeMap(startx, starty, 100, 10, 30)
+    themap.printt()
